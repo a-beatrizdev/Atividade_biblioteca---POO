3d597ef844500a35cb5b88922d1ddb7ec643f3a6
Alana <alanabeatriztobias@gmail.com>
2026-03-06 08:44:49 -0300

    Criando def com get e set da classe Revista

--- revista.py
+++ revista.py
@@ -5,3 +5,27 @@ class Revista(Itembiblioteca):
         super().__init__(codigo, titulo, ano)
         self.__edicao = edicao
         self.__mes = mes
+
+    def get_edicao(self):
+        return self.__edicao
+    
+    def get_mes(self):
+        return self.__mes
+    
+    def set_edicao(self, edicao):
+        if edicao:
+            self.__edicao = edicao
+        else:
+            raise ValueError("Edição deve ser maior que zero")
+    def set_mes(self, mes):
+        if mes:
+            self.__mes = mes
+        else: 
+            raise ValueError("O mês não pode ser vazio")
+        
+    def exibir_detalhes(self):
+        status = "Disponível" if self.get_disponivel() else "Emprestado"
+        print(f"[Revista] Código: {self.get_codigo()} | Título: {self.get_titulo()} | "
+              f"Ano: {self.get_ano()} | Edição: {self.__edicao} | "
+              f"Mês: {self.__mes} | Status: {status}")
+        
