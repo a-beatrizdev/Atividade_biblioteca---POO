cde02e76697824fbff0567fca170ecf8026f22eb
Alana <alanabeatriztobias@gmail.com>
2026-03-06 08:31:25 -0300

    Cria classe ItemBiblioteca com encapsulamento

--- item_biblioteca.py
+++ item_biblioteca.py
@@ -1,2 +1,9 @@
 # Classe base da biblioteca 
 # Ela será herdada por Livro e Revista
+
+class Itembiblioteca:
+    def __init__(self, codigo: int, titulo: str, ano: int):
+        self.__codigo = codigo
+        self.__titulo = titulo
+        self.__ano = ano
+        self.__disponivel = True
