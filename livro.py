521f3d5b9e86833bb5db84e63357a0f5f0bd7687
Alana <alanabeatriztobias@gmail.com>
2026-03-06 08:41:05 -0300

    Criando def com get e set

--- livro.py
+++ livro.py
@@ -5,3 +5,27 @@ class Livro(Itembiblioteca):
         super().__init__(codigo, titulo, ano)
         self.__autor = autor
         self.__num_paginas = num_paginas
+
+    def get_autor(self):
+        return self.__autor
+    
+    def get_num_paginas(self):
+        return self.__num_paginas
+    
+    def set_autor(self, novo_autor):
+        if novo_autor:
+            self.__autor = novo_autor
+        else:
+            raise ValueError("O nome do autor não pode ficar vazio")
+    
+    def set_num_paginas(self, novo_num_paginas):
+        if novo_num_paginas:
+            self.__num_paginas = novo_num_paginas
+        else:
+            raise ValueError("O número de paginas tem que ser maior que zero")
+    
+    def exibir_detalhes(self):
+        status = "Disponível" if self.get_disponivel() else "Emprestado"
+        print(f"[Livro] Código: {self.get_codigo()} | Título: {self.get_titulo()} | "
+              f"Ano: {self.get_ano()} | Autor: {self.__autor} | "
+              f"Páginas: {self.__num_paginas} | Status: {status}")
